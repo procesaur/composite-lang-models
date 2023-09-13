e29c336a47faa764030384207637f4a938006c5f
Mihailo Škorić <31268468+procesaur@users.noreply.github.com>
2023-09-13 14:23:28 +0200

    Update pearson.py

--- pearson.py
+++ pearson.py
@@ -5,10 +5,28 @@ import pandas as pd
 from scipy.stats import pearsonr
 
 
-def flatten_dict(d: MutableMapping, parent_key: str = '', sep: str ='.') -> MutableMapping:
+def map_outer(key):
+    if key == "procesaur/gpt2-srlat":
+        return "m1"
+    if key == "procesaur/gpt2-srlat-sem":
+        return "m2"
+    if key == "procesaur/gpt2-srlat-synt":
+        return "m3"
+
+
+def map_inner(key):
+    if key == "sr":
+        return "t1"
+    if key == "bad":
+        return "t2"
+    if key == "google":
+        return "t3"
+
+
+def flatten_dict(d: MutableMapping, parent_key: str = '', sep: str = '.') -> MutableMapping:
     items = []
     for k, v in d.items():
-        new_key = parent_key.replace("procesaur/", "") + sep + k if parent_key else k
+        new_key = map_outer(parent_key) + sep + map_inner(k) if parent_key else k
         if isinstance(v, MutableMapping):
             items.extend(flatten_dict(v, new_key, sep=sep).items())
         else:
@@ -16,25 +34,31 @@ def flatten_dict(d: MutableMapping, parent_key: str = '', sep: str ='.') -> Muta
     return dict(items)
 
 
+def fill_and_print(cols):
+    for col in cols:
+        df = pd.DataFrame(0, columns=col, index=col)
+        for x in col:
+            for y in col:
+                if x == y:
+                    df[x][y] = 1
+                else:
+                    if df[y][x] > 0:
+                        df[x][y] = df[y][x]
+                    else:
+                        p, _ = pearsonr(probs[x], probs[y])
+                        df.loc[x, y] = round(p, 3)
+        print(df.to_csv(sep="\t"))
+
+
 with open("data/probabilities.json", "r") as jf:
     probs = flatten_dict(load(jf))
 
-cols_sr = [x for x in probs if ".sr" in x]
-cols_bad = [x for x in probs if ".bad" in x]
-cols_google = [x for x in probs if ".google" in x]
+cols_t1 = sorted([x for x in probs if ".t1" in x])
+cols_t2 = sorted([x for x in probs if ".t2" in x])
+cols_t3 = sorted([x for x in probs if ".t3" in x])
+cols_m1 = sorted([x for x in probs if "m1." in x])
+cols_m2 = sorted([x for x in probs if "m2." in x])
+cols_m3 = sorted([x for x in probs if "m3." in x])
 
-cols = [cols_sr, cols_bad, cols_google, probs]
-
-for col in cols:
-    df = pd.DataFrame(0, columns=col, index=col)
-    for x in col:
-        for y in col:
-            if x == y:
-                df[x][y] = 1
-            else:
-                if df[y][x] > 0:
-                    df[x][y] = df[y][x]
-                else:
-                    p, _ = pearsonr(probs[x], probs[y])
-                    df.loc[x, y] = p
-    print(df.to_string())
+fill_and_print([cols_t1, cols_t2, cols_t3])
+fill_and_print([cols_m1, cols_m2, cols_m3])
